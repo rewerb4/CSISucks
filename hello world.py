a3cfa8616849f848624f9b6a5e9b541c7a108b20
rewerb4 <mbrewe13@bruinmail.slcc.edu>
2014-09-18 20:00:20 -0600

    Update hello world.py

--- hello world.py
+++ hello world.py
@@ -1,7 +1,21 @@
 #creating new file for repository
 def response(string):
+  resp = ''
+    if string == 'good':
+        resp = 'sweet'
+        return resp
+    elif string == 'bad':
+        resp = 'That sucks hommie'
+        return resp
+    elif string == 'meh':
+        resp = 'time for a drink ;-)'
+        return resp
+    
+    else:
+        resp = 'I did ask for that'
+        return resp
   
   
 print('hello world)
-user_input = raw_input('\nHow is your day?: ').lower()
-reponse(user_input)
+user_input = raw_input('\nHow is your day? Good, Bad, Meh: ').lower()
+print (reponse(user_input))
